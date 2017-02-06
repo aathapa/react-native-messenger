94a3f51ea9212131f76652c7a1154bd90a223b8d
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-06 19:30:02 +0545

    reusable components

---
+++ src/components/MyStatusBar/MyStatusBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Text,View,StatusBar,Platform } from 'react-native';
+import { Colors } from '../theme';
+
+export default class MyStatusBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+             <View style={styles.statusBar}>
+                 <StatusBar backgroundColor="#0084ff" barStyle='light-content' />
+            </View>
+        );
+    }
+}
+
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : 0;
+
+const styles = {
+    statusBar: {
+        height: STATUSBAR_HEIGHT,
+        backgroundColor: Colors.primaryColors
+    }
+}
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import MyStatusBar from './MyStatusBar/MyStatusBar'
+
+export default { MyStatusBar };
