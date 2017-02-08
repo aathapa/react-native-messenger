be8d93d8b8d05fb6e9be99aa402109fae38bfe5e
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-08 18:45:51 +0545

    navbar height assign to platform

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text,View } from 'react-native';
+import styles from './NavBarStyle';
+
+
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <View style={styles.navContainer}>
+
+            </View>
+        );
+    }
+}
---
+++ src/components/NavBar/NavBarStyle.js
@@ -0,0 +1,8 @@
+import { Colors,Metrics } from '../../theme/';
+
+export default styles = {
+    navContainer: {
+        height: Metrics.Metrics.NAVBAR_HEIGHT,
+        backgroundColor: Colors.secondaryColor
+    }
+}
