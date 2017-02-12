1622363ddcb815c668df18d6f4aed2c80ad44c44
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-12 12:57:29 +0545

    NavBar shadow used

--- src/components/NavBar/NavBarStyle.js
+++ src/components/NavBar/NavBarStyle.js
@@ -3,6 +3,11 @@ import { Colors,Metrics } from '../../theme/';
 export default styles = {
     navContainer: {
         height: Metrics.Metrics.NAVBAR_HEIGHT,
-        backgroundColor: Colors.secondaryColor
+        backgroundColor: Colors.secondaryColor,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 3},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 1,
     }
 }
