44497ea691b63a7a2ac0bc43a5295e4883ab0d27
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-12 13:14:03 +0545

    Card used in project

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+}
+
+export default Card;
