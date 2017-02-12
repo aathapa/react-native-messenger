2cbf1b752920477dd5599c6bb81d2cd724839e1f
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-12 14:48:32 +0545

    images added

--- src/theme/Images.js
+++ src/theme/Images.js
@@ -1,3 +1,15 @@
 export default Images= {
-    messengerLogo: require('../images/messenger.png')
+    messengerLogo: require('../images/messenger.png'),
+    bibekPic: require('../images/bibek.jpg'),
+    deepakPic: require('../images/deepak.jpg'),
+    bsaalPic: require('../images/bsaal.jpg'),
+    bishalShiwaPic: require('../images/bishal_sh.jpg'),
+    soodepDhakalPic: require('../images/sudip_dh.jpg'),
+    sudeepAcharyaPic: require('../images/sudeep.jpg'),
+    pamitPic: require('../images/pamit.jpg'),
+    bijayPic: require('../images/bijay.jpg'),
+    tirthaPic: require('../images/tirtha.jpg'),
+    saileshPic: require('../images/sailesh.jpg'),
+    maheshPic: require('../images/mahesh.jpg'),
+    topPic: require('../images/top.jpg')
 }
