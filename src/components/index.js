93a95a4798f56730e3295862ff6c844899cc2cb6
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-12 13:14:11 +0545

    card exported

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import MyStatusBar from './MyStatusBar/MyStatusBar'
 import NavBar from './NavBar/NavBar';
+import Card from './Card/Card';
 
-export  { MyStatusBar,NavBar };
+export  { MyStatusBar,NavBar,Card };
