339d15d877de76494df68910c7554e2717f950ff
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-12 13:35:36 +0545

    card styles customized

--- src/components/Card/CardStyle.js
+++ src/components/Card/CardStyle.js
@@ -1,17 +1,13 @@
 const styles = {
   containerStyle: {
-    borderWidth: 1,
-    borderRadius: 2,
+    backgroundColor: '#fff',
     borderColor: '#ddd',
-    borderBottomWidth: 0,
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.1,
     shadowRadius: 2,
     elevation: 1,
-    marginLeft: 5,
-    marginRight: 5,
-    marginTop: 10
+    marginTop: 5
   }
 }
 export default styles;
