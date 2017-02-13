66fba45a58bad2bf467b24c11cf9244ff6846111
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-13 14:03:00 +0545

    acount list displayed

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { Text,View } from 'react-native';
+import { Text,View,ListView,ScrollView } from 'react-native';
 import styles from './HomeStyle';
 import { MyStatusBar,NavBar,Card } from '../../components/';
+import { Images } from '../../theme/';
+import { AccountList } from '../../containers/';
+    
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
     }
+    /*
+    renderAccountList() {
+        return favouriteAccount.map((favouritelist) =>
+            <AccountList key={favouritelist.title} favouritelist={favouritelist} />
+        );
+    }
+    */
+   
     render() {
         return (
             <View style={styles.homeContainer}>
@@ -16,11 +27,10 @@ export default class Home extends Component {
                <View style={styles.NavBarContainer}>
                     <NavBar />
                </View>
-                <View>
-                    <Card>
-                        <Text>saf</Text>
-                    </Card>
-                </View>
+               <ScrollView>
+                   <AccountList />
+               </ScrollView>
+               
                 
             </View>
         );
