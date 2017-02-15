c4687724858e699a9f75a2773b29f8b4a93c7f39
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-15 16:59:24 +0545

    call content added

--- src/containers/Call/Call.js
+++ src/containers/Call/Call.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Text,View } from 'react-native';
+import { Text,View,TouchableOpacity,ScrollView } from 'react-native';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 import styles from './CallStyle';
-import { MyStatusBar } from '../../components/';
+import { MyStatusBar,NavBar,AccountChat,Card } from '../../components/';
+import { Data,Colors } from '../../theme/'
 
 
 export default class Call extends Component {
@@ -11,7 +13,94 @@ export default class Call extends Component {
     render() {
         return (
             <View style={styles.callContainer}>
-                <MyStatusBar />
+                <View>
+                    <MyStatusBar />
+                </View>
+                <View>
+                    <NavBar />
+                </View>
+                <TouchableOpacity style={styles.groupCall}>
+                    <View style={styles.groupIcon}>
+                        <IonIcons name="md-people" size={22} color={Colors.primaryColor} />
+                    </View>
+                    <View style={styles.groupText}>
+                        <Text style={styles.callText} >Start Group Call</Text>
+                    </View>
+                </TouchableOpacity>
+                <View style={styles.suggestContainer}>
+                    <Text style={styles.suggestText}>SUGGESTED</Text>
+                </View>
+                <ScrollView>
+                <View>
+                    <AccountChat 
+                        image={Data.favouritelist.deepak.images}
+                        title={Data.favouritelist.deepak.title}
+                        username={Data.favouritelist.deepak.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.favouritelist.bishal.images}
+                        title={Data.favouritelist.bishal.title}
+                        username={Data.favouritelist.bishal.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.favouritelist.mahesh.images}
+                        title={Data.favouritelist.mahesh.title}
+                        username={Data.favouritelist.mahesh.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.favouritelist.top.images}
+                        title={Data.favouritelist.top.title}
+                        username={Data.favouritelist.top.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.birthdaylist.pamit.images}
+                        title={Data.birthdaylist.pamit.title}
+                        username={Data.birthdaylist.pamit.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.bibek.images}
+                        title={Data.moreconversation.bibek.title}
+                        username={Data.moreconversation.bibek.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.bijay.images}
+                        title={Data.moreconversation.bijay.title}
+                        username={Data.moreconversation.bijay.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.bishalShiwa.images}
+                        title={Data.moreconversation.bishalShiwa.title}
+                        username={Data.moreconversation.bishalShiwa.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.sailesh.images}
+                        title={Data.moreconversation.sailesh.title}
+                        username={Data.moreconversation.sailesh.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.sudipDhakal.images}
+                        title={Data.moreconversation.sudipDhakal.title}
+                        username={Data.moreconversation.sudipDhakal.username}
+                        type="call"
+                    />
+                    <AccountChat 
+                        image={Data.moreconversation.sudeepAcharya.images}
+                        title={Data.moreconversation.sudeepAcharya.title}
+                        username={Data.moreconversation.sudeepAcharya.username}
+                        type="call"
+                    />
+                </View>
+                                    
+                </ScrollView>
             </View>
         );
     }
--- src/containers/Call/CallStyle.js
+++ src/containers/Call/CallStyle.js
@@ -1,8 +1,31 @@
-import { Colors } from '../../theme/';
+import { Colors,Fonts } from '../../theme/';
 
 export default styles = {
     callContainer: {
         flex: 1,
-        backgroundColor: Colors.secondaryWhiteColor
+        backgroundColor: Colors.secondaryWhiteColor,
+    },
+    groupCall: {
+        padding: 10,
+        flexDirection: 'row',
+        marginTop: 5
+    },
+    groupIcon: {
+        flex:1.3
+    },
+    groupText: {
+        flex:10,
+        
+    },
+    callText: {
+        fontFamily: Fonts.type.regular
+    },
+    suggestContainer: {
+        padding: 10
+    },
+    suggestText: {
+        color: Colors.secondaryBlackColor,
+        fontSize: Fonts.size.small,
+        fontWeight: "700"
     }
 }
