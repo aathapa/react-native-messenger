c23ac2addaff458dae193d21c49ea9abca556083
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-15 16:59:38 +0545

    data added

--- src/theme/Data.js
+++ src/theme/Data.js
@@ -5,32 +5,37 @@ const favouritelist = {
         images: require('../images/deepak.jpg'), 
         title: "Deepak Poudel",
         time: "3:41 pm",
-        message: "Pokhara tirai"
+        message: "Pokhara tirai",
+        username: "@deepak"
     },
     bishal: {
         images: require('../images/bsaal.jpg'), 
         title: "Bishal Giri",
         time: "12:15 pm",
-        message: "Ktm ma"
+        message: "Ktm ma",
+        username: "@bishal"
     },
     mahesh: {
         images: require('../images/mahesh.jpg'), 
         title: "Mahesh Joshi",
         time: "12:05 pm",
-        message: "Ho yar"
+        message: "Ho yar",
+        username: "@mahesh"
     },
     top: {
         images: require('../images/top.jpg'), 
         title: "Top Pun",
         time: "11:15 am",
-        message: "Auddda"
+        message: "Auddda",
+        username: "@top"
     }
 }
 const birthdaylist = {
     pamit: {
         images: require('../images/pamit.jpg'),
         title: "Pamit Acharya",
-        message : "Wish him a happy birthday"
+        message : "Wish him a happy birthday",
+        username: "@pamit"
     }
 }
 
@@ -39,43 +44,50 @@ const moreconversation = {
         images: require('../images/bibek.jpg'),
         title: "Bibek Gupta",
         time: "10:15 am",
-        message: "ghatti"
+        message: "ghatti",
+        username: "@bibek"
     },
     sudipDhakal: {
         images: require('../images/sudip_dh.jpg'),
         title: "Soodep Dhakal",
         time: "9:15 am",
-        message: "Tei ta yr"
+        message: "Tei ta yr",
+        username: "@sudipdhakal"
     },
     sailesh: {
         images: require('../images/sailesh.jpg'), 
         title: "Sailesh Acharya",
         time: "8:15 pm",
-        message: "dosti"
+        message: "dosti",
+        username: "@sailesh"
     },
     bijay: {
         images: require('../images/bijay.jpg'), 
         title: "Bijay Gurung",
         time: "Sun",
-        message: "USA"
+        message: "USA",
+        username: "@bijay"
     },
     sudeepAcharya: {
         images: require('../images/sudeep.jpg'), 
         title: "Sudeep Acharya",
         time: "Sat",
-        message: "Sainik Sasan "
+        message: "Sainik Sasan ",
+        username: "@sudeepacharya"
     },
     bishalShiwa: {
         images: require('../images/bishal_sh.jpg'), 
         title: "Bishal Shiwa",
         time: "Fri",
-        message: "Aja match herne ho ? "
+        message: "Aja match herne ho ? ",
+        username: "@bishalsh"
     },
     tirtha: {
         images: require('../images/tirtha.jpg'),
         title: "Tirtha Basnet",
         time: "Fri",
-        message: "k garnu ta"
+        message: "k garnu ta",
+        username: "@tirtha"
     }
 }
 
